de8e0c1eb6416c05daa8b22211dd2c0f3fbaf4b8
Sean Wong <seanwong24@live.com>
2020-07-01 04:39:08 +0000

    remove unwanted initial files

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -9,9 +9,8 @@ export class AppRoot {
   render() {
     return (
       <ion-app>
-        <ion-router useHash={false}>
+        <ion-router useHash={true}>
           <ion-route url="/" component="app-home" />
-          <ion-route url="/profile/:name" component="app-profile" />
         </ion-router>
         <ion-nav />
       </ion-app>
