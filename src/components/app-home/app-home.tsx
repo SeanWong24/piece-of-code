3c1f47e9a3c508703e298df9330d0261d7d690f8
Sean Wong <seanwong24@live.com>
2020-07-02 02:15:44 +0000

    use deflated buffer

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,5 +1,6 @@
 import { Component, ComponentInterface, Host, h, Prop } from '@stencil/core';
 import monaco from '@timkendrick/monaco-editor';
+import pako from 'pako';
 
 @Component({
   tag: 'app-home',
@@ -14,10 +15,17 @@ export class AppHome implements ComponentInterface {
   @Prop() data: string;
 
   componentDidLoad() {
+    let result = '';
+    if (this.data) {
+      const encodedBuffer = this.base64ToBuffer(this.data);
+      const inflatedBuffer = pako.inflate(encodedBuffer);
+      result = new TextDecoder('utf8').decode(inflatedBuffer);
+    }
+
     this.monacoEditor = monaco.editor.create(
       this.editorContainerElement,
       {
-        value: atob(this.data?.replace(/-/g, '/') || ''),
+        value: result.replace(/-/g, '/'),
         language: this.language,
         automaticLayout: true
       }
@@ -43,7 +51,10 @@ export class AppHome implements ComponentInterface {
             </ion-select>
             <ion-buttons slot="end">
               <ion-button onClick={() => {
-                const url = `${document.URL.split('#')[0]}#/${this.language}/${btoa(this.monacoEditor.getValue()).replace(/\//g, '-')}`;
+                const text = this.monacoEditor.getValue();
+                const deflatedText = pako.deflate(new TextEncoder().encode(text));
+                const base64String = this.bufferToBase64(deflatedText);
+                const url = `${document.URL.split('#')[0]}#/${this.language}/${base64String.replace(/\//g, '-')}`;
                 window.location.href = url;
               }}>Apply URL</ion-button>
             </ion-buttons>
@@ -54,6 +65,22 @@ export class AppHome implements ComponentInterface {
         </ion-content>
       </Host>
     );
+  }
+
+  bufferToBase64(buffer: Uint8Array) {
+    var binstr = Array.prototype.map.call(buffer, function (character) {
+      return String.fromCharCode(character);
+    }).join('');
+    return btoa(binstr);
+  }
+
+  base64ToBuffer(base64: string) {
+    var binstr = atob(base64);
+    var buffer = new Uint8Array(binstr.length);
+    Array.prototype.forEach.call(binstr, function (character, i) {
+      buffer[i] = character.charCodeAt(0);
+    });
+    return buffer;
   }
 
 }
